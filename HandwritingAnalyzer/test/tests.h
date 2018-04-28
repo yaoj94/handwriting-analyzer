bb17835f1b35eca7a1abbaca3e1d67efc037d4c9
yaoj94 <yaoj94@gmail.com>
2018-04-28 03:27:26 -0500

    added test files

---
+++ HandwritingAnalyzer/test/tests.h
@@ -0,0 +1,11 @@
+#ifndef tests_h
+#define tests_h
+
+#define CATCH_CONFIG_RUNNER
+#include "catch.hpp"
+#include <stdio.h>
+#include "strokes.h"
+#include "classifier.h"
+#include "factors.h"
+
+#endif /* tests_h */
